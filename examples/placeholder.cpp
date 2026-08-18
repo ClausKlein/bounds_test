1a4f8fd42c8ee06efb4f820ad51bf5b4bf7ad7d9
ClausKlein <claus.klein@arcormail.de>
2026-08-18 10:19:31 +0200

    Try as header only library
    Use static_assert on examples
    
    Fix clang-tidy warnings
    
    Update .gitignore

--- examples/placeholder.cpp
+++ examples/placeholder.cpp
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#ifdef __INTELLISENSE__
+#include <cassert> // Add this for runtime assert
+#include <limits>
+
 #include <beman/bounds_test/bounds_test.hpp>
-#else
-import beman.bounds_test;
-#endif
+// Alternatively: import beman.bounds_test;
 
 namespace bt = beman::bounds_test;
 
 int main(int argc, char** /* argv */) {
-  return bt::can_convert_modular<unsigned>(argc);
+  [[maybe_unused]] constexpr int cnt{std::numeric_limits<int>::max()};
+
+  // TODO(CK): Change static_assert to standard runtime assert
+  static_assert(bt::can_convert_modular<unsigned>(cnt));
+  static_assert(bt::can_convert<unsigned>(cnt));
 }
